209cc4247a1520024c23676909f8a6883591dc47
davidluz <davidmelo8@hotmail.com>
2018-08-28 21:28:17 -0300

    v 0.1

---
+++ fgvapp1/app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+router.get('/userlist', function(req, res) {
+   var db = require("../db");
+   var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
+   Users.find({}).lean().exec(
+      function (e, docs) {
+         res.render('userlist', { "userlist": docs });
+   });
+});
+
+module.exports = app;
